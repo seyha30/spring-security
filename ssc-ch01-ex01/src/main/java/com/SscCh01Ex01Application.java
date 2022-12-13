c47379eb3867b10fe367d7fc4a92b6aad56512d8
seyha30 <sinseyha1x@gmail.com>
2022-12-13 23:13:45 +0700

    new

---
+++ ssc-ch01-ex01/src/main/java/com/SscCh01Ex01Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscCh01Ex01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SscCh01Ex01Application.class, args);
+	}
+
+}
